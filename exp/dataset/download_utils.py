dd242a9b036d3ffbe7e0fc0a1d9d7a79d9d783d1
Leon <king0980682@gmail.com>
2021-11-22 03:30:09 +0000

    Init exp folder

---
+++ exp/dataset/download_utils.py
@@ -0,0 +1,24 @@
+from contextlib import contextmanager
+
+@contextmanager
+def download_path(path=None):
+    """Return a path to download data. If `path=None`, then it yields a temporal path that is eventually deleted,
+    otherwise the real path of the input.
+    Args:
+        path (str): Path to download data.
+    Returns:
+        str: Real path where the data is stored.
+    Examples:
+        >>> with download_path() as path:
+        >>> ... maybe_download(url="http://example.com/file.zip", work_directory=path)
+    """
+    if path is None:
+        tmp_dir = TemporaryDirectory()
+        try:
+            yield tmp_dir.name
+        finally:
+            tmp_dir.cleanup()
+    else:
+        path = os.path.realpath(path)
+        yield path
+
---
+++ exp/dataset/movielens.py
@@ -0,0 +1,12 @@
+from download_utils import download_path 
+
+def load_movielens(path):
+    
+    '''
+        https://files.grouplens.org/datasets/movielens/ml-100k.zip
+    '''
+
+    filepath = os.path.join(path, "ml-{}.zip".format(size))
+    
+
+
